95aff17c3dae8858a34f1639c58b4cd2eef19bb9
Nile Camai <nile.camai@gmail.com>
2019-03-16 22:04:23 -0700

    Successfully Generate gotomobile Go Bindings

---
+++ NileTriesThings/src/gotomobile/gotomobile.go
@@ -0,0 +1,31 @@
+package gotomobile
+
+import "fmt"
+
+func SayHello() {
+	fmt.Println("Hello!")
+}
+
+func SayHelloWithName(name string) {
+	fmt.Println("Hello! " + name)
+}
+
+// this returns an error if name is not x
+func ThisReturnsAnError(name string) error {
+	if name != "x" {
+		return fmt.Errorf("name is not x")
+	}
+	return nil
+}
+
+func Returns2Arguments(firstName, LastName string) (string, error) {
+	if LastName != "x" {
+		return "", fmt.Errorf("eerror")
+	}
+	return LastName + LastName, nil
+}
+
+// This will not work. Second result value must be of type error
+//func Returns2Arguments(firstName, LastName string) (string, string) {
+// return LastName, firstName
+//}
